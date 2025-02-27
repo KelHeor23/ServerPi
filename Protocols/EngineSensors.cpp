9039d3355f4665c91e9026bbb8e9d53496530205
Alex <kelheor23@gmail.com>
2025-02-27 18:03:23 +0300

    add rand

--- Protocols/EngineSensors.cpp
+++ Protocols/EngineSensors.cpp
@@ -9,10 +9,10 @@ EngineSensors::EngineSensors() {}
 std::string EngineSensors::generateMsg(uint32_t canID)
 {
     data.canID = data.canID = Protocol_numbers::ENGINE_SENSORS | (canID & 0x7);;
-    data.speed = 0;//rand() % (65536);
-    data.temperature = 0;//static_cast<int8_t>(rand() % (256) - 128);
-    data.runoutAngle = 0;//rand() % (360);
-    data.runoutAmplitude = 0;//rand() % (65536);
+    data.speed = rand() % (65536);
+    data.temperature = static_cast<int8_t>(rand() % (256) - 128);
+    data.runoutAngle = rand() % (360);
+    data.runoutAmplitude = rand() % (65536);
 
     std::string str(reinterpret_cast<char *> (&data), sizeof(data));
 
