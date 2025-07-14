2d792571e92989a8ff72c9d318494422b2dbec54
Alex <kelheor23@gmail.com>
2025-07-14 12:58:13 +0300

    run fix

--- run.sh
+++ run.sh
@@ -2,4 +2,16 @@ cmake -Bbuild -H.
 cmake --build build -j4
 cd build
 make
-./ServerPi
+
+sudo slcand -o -c -s8 /dev/ttyACM0 can0
+sudo ip link set up can0
+
+# Запуск программы с гарантированным завершением
+trap 'kill $PID' SIGINT SIGTERM
+./ServerPi &
+PID=$!
+wait $PID
+
+# Остановка CAN-интерфейса после программы
+sudo ip link set down can0
+sudo pkill -f 'slcand .* /dev/ttyACM0'
--- slcan_run.sh
+++
@@ -1,2 +0,0 @@
-sudo slcand -o -c -s8 /dev/ttyACM0 can0
-sudo ip link set up can0
